6afd53641c5611ae5289dbc296c043caa67e0b75
sayonUsman <78254224+sayonUsman@users.noreply.github.com>
2021-02-09 18:35:54 +0600

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	typedef char string[30];
+
+	string name;
+
+	int length = 0;
+	
+	int index;
+
+	scanf("%[^\n]", &name);
+
+	while (name[length] != '\0')
+	{
+		length++;
+	}
+	
+	index = length - 1;
+	
+	printf("\nThe reverse string is ");
+	
+	for(; index >= 0; index --)
+	{
+		printf("%c", name[index]);
+	}
+	
+	printf("\n");
+
+	return 0;
+}
